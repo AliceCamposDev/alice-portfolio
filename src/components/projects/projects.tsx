252d701bbf0fc900aa0b4fb23b6b2cb2f9cb99fb
Alice Campos <allicefcampos@gmail.com>
2026-02-05 20:33:06 -0300

    feat: add live demo for rpg backend

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -21,9 +21,9 @@ export default function Projects() {
       <div className="projectGrid">
         <ProjectCard title="My portfolio" subtitle="React website to show my projects" img={kuromi} links={[{name: "GitHub", url: "https://github.com/AliceCamposDev/alice-portfolio"},{name: "Live demo", url:"https://alice-portfolio-five.vercel.app/"}]}/>
         <ProjectCard title="Guerrilha Urbana" subtitle="Angular website for a TTRPG game" img={d20} links={[{name: "GitHub", url: "https://github.com/AliceCamposDev/rpg-guerrilha-urbana"},{name: "Live demo", url:"https://alicecamposdev.github.io/rpg-guerrilha-urbana/"}]}/>
-        <ProjectCard title="Guerrilha Urbana Backend" subtitle="Python fast api for Guerrilha Urbana rpg" img={d4} links={[{name: "GitHub", url: "https://github.com/AliceCamposDev/rpg-guerrilha-urbana-back"}]}/>
+        <ProjectCard title="Guerrilha Urbana Backend" subtitle="Python fast api for Guerrilha Urbana rpg" img={d4} links={[{name: "GitHub", url: "https://github.com/AliceCamposDev/rpg-guerrilha-urbana-back"} ,{name: "Live demo", url:"https://rpg-guerrilha-urbana-back-fastapi.vercel.app/"}]}/>
         <ProjectCard title="Obsidian LLM RAG" subtitle="Context retrieve and RAG implementation for Obsidian vaults" img={obsidianLogo} links={[{name: "GitHub", url: "https://github.com/AliceCamposDev/obsidian-rag"}]}/>
-        <ProjectCard title="Rexx Network coin" subtitle="Cryptocurrency" img={placeholder} />
+        {/* <ProjectCard title="Rexx Network coin" subtitle="Cryptocurrency" img={placeholder} /> */}
         <ProjectCard title="My New Project" subtitle="React App" img={placeholder} />
 
       </div>
